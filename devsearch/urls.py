aeb78142a274daaf839cb426e08c4af676221a56
LittleBird-yg <littlebird.yg@gmail.com>
2022-12-06 19:02:08 -0800

    Added changes to project app

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -15,18 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
 
-
-def projects(request):
-    return HttpResponse('Here are our Projects')
-
-def project(request, pk):
-    return HttpResponse('SINGLE PROJECT' + ' ' + str(pk))
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projects/', projects, name="projects"),
-    path('project/<str:pk>', project, name="project"),
-
 ]
